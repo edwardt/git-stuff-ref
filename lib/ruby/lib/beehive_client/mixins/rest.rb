dc1520c63c48d4781310f3f008018627e801af0e
Jacob Dunphy <jacob.dunphy@gmail.com>
2010-11-09 13:21:42 -0800

    indent fix BeehiveClient::Rest module.

--- lib/ruby/lib/beehive_client/mixins/rest.rb
+++ lib/ruby/lib/beehive_client/mixins/rest.rb
@@ -4,7 +4,7 @@ require "json/pure"
 module BeehiveClient
   module Rest
 
-     # REST Methods
+    # REST Methods
     def get(path, params = {})
       uri = "http://#{host}/#{path}"
       r = unless params.empty?
@@ -15,38 +15,38 @@ module BeehiveClient
       JSON.parse(r)
     end
 
-     def post(path, params={})
-       j = RestClient.post("http://#{host}/#{path}", params.to_json)
-       handle_response(j)
-     end
+    def post(path, params={})
+      j = RestClient.post("http://#{host}/#{path}", params.to_json)
+      handle_response(j)
+    end
 
-     def put(path, params={})
-       j = RestClient.put("http://#{host}/#{path}", params.to_json)
-       handle_response(j)
-     end
+    def put(path, params={})
+      j = RestClient.put("http://#{host}/#{path}", params.to_json)
+      handle_response(j)
+    end
 
-     def delete(path)
-       j = RestClient.delete("http://#{host}/#{path}")
-       handle_response(j)
-     end
+    def delete(path)
+      j = RestClient.delete("http://#{host}/#{path}")
+      handle_response(j)
+    end
 
 
-     private
+    private
 
-     def handle_response(resp)
-       r = JSON.parse(resp)
-        if r["error"]
-          if r["error"] == "There was a problem authenticating"
-            raise StandardError.new("
+    def handle_response(resp)
+      r = JSON.parse(resp)
+      if r["error"]
+        if r["error"] == "There was a problem authenticating"
+          raise StandardError.new("
   There was an error authenticating
   Check your credentials
             ")
-          else
-            raise StandardError.new(r["error"])
-          end
         else
-          return r
+          raise StandardError.new(r["error"])
         end
-     end
+      else
+        return r
+      end
+    end
   end
 end
