ca8e295eaf5154e1c757ca553a69e49587ebcab3
Jacob Dunphy <jacob.dunphy@gmail.com>
2010-11-11 09:41:37 -0800

    Adding in profile page to dashboard.

--- priv/www/js/controllers/users_controller.js
+++ priv/www/js/controllers/users_controller.js
@@ -3,7 +3,7 @@
 //------------------//
 
 var users_controller = function(app) {
-  
+
   this.get("#/users", function(context) {
     this.get_page("/users.json", "users");
     this.users_list = [];
@@ -12,5 +12,14 @@ var users_controller = function(app) {
       this.users_list.push(user);
     }
   });
-  
+
+  this.get("#/profile", function(context) {
+    var email = Sammy.current_user["user"];
+    this.get_page("/users/" + email + ".json", "user");
+    this.get_page("/users/" + email + "/apps.json", "apps");
+    this.template = "users/show";
+    console.log(this.user);
+    console.log(this.apps);
+  });
+
 }
