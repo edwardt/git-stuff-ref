185f4b6d8ba136d76dedcbaa76096693a3b899f4
Jacob Dunphy <jacob.dunphy@gmail.com>
2010-12-17 14:51:40 -0800

    adding doc to gitignore and adding some doc to beehive_repository interface

--- lib/erlang/apps/beehive/src/bh_git/beehive_repository.erl
+++ lib/erlang/apps/beehive/src/bh_git/beehive_repository.erl
@@ -9,34 +9,78 @@
 -behaviour(gen_server).
 
 %% API
--export([start_link/0]).
-
-%% gen_server callbacks
--export([init/1, handle_call/3, handle_cast/2, handle_info/2,
-         terminate/2, code_change/3]).
-
--export([
+-export([start_link/0,
          create/1,
          add_user_to_repository/2,
          remove_user_from_repository/2,
          clone/2,
          clone_url/1,
          add_user_pubkey/2
-        ]).
+]).
+
+%% gen_server callbacks
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2,
+         terminate/2, code_change/3]).
 
 -record(state, { name, pid }).
 -define(SERVER, ?MODULE).
 
+%%--------------------------------------------------------------------
+%% @doc  Set up a repository in the git backend
+%% @spec create(Name) -> ok | {error, Error}
+%% @end
+%%--------------------------------------------------------------------
 create(Name) ->
   gen_server:call(?SERVER, {create, Name}).
+
+%%--------------------------------------------------------------------
+%% @doc
+%% If git backend has permission requirements (like gitolite), then
+%% add configuration to provide user with RW permissions.
+%%
+%% @spec add_user_to_repository(Username, Name) -> ok | {error, Error}
+%% @end
+%%--------------------------------------------------------------------
 add_user_to_repository(Username, Name) ->
   gen_server:call(?SERVER, {add_user, Username, Name}).
+
+%%--------------------------------------------------------------------
+%% @doc
+%% If git backend has permission requirements (like gitolite), then
+%% updated config to remove RW permissions for user.
+%%
+%% @spec remove_user_from_repository(Username, Name) ->
+%          ok | {error, Error}
+%% @end
+%%--------------------------------------------------------------------
 remove_user_from_repository(Username, Name) ->
   gen_server:call(?SERVER, {remove_user, Username, Name}).
+
+%%--------------------------------------------------------------------
+%% @doc  Performs a git clone of a repository, using Path as its
+%%       destination in the file system.
+%% @spec clone(Name, Path) -> ok | {error, Error}
+%% @end
+%%--------------------------------------------------------------------
 clone(Name, Path) ->
   gen_server:call(?SERVER, {clone, Name, Path}).
+
+%%--------------------------------------------------------------------
+%% @doc  Provides either a ssh or local filesystem path that can be used
+%%       as a git remote for pushing to the git backend.
+%% @spec clone_url(Name) -> {ok, Url} | {error, Error}
+%% @end
+%%--------------------------------------------------------------------
 clone_url(Name) ->
   gen_server:call(?SERVER, {clone_url, Name}).
+
+%%--------------------------------------------------------------------
+%% @doc  Save a public ssh key for a given user Name.
+%% @spec add_user_pubkey(Name, Key) -> ok | {error, Error}
+%%          Name = string()
+%%          Key  = binary()
+%% @end
+%%--------------------------------------------------------------------
 add_user_pubkey(Name, Key) ->
   gen_server:call(?SERVER, {add_pubkey, Name, Key}).
 
