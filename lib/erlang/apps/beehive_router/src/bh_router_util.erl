07b4db2b5cc52eeb46a15d8683f78f65effb0e7e
ed t <edwardt.tril@gmail.com>
2011-01-10 16:33:56 -0800

    =add

---
+++ lib/erlang/apps/beehive_router/src/bh_router_util.erl
@@ -0,0 +1,42 @@
+-module(bh_router_util).
+
+-export([notify_manager/1]).
+
+-export([ensure_loaded/1, ensure_deps_loaded/1]).
+
+notify_manager(Event)-> node_manager:notify(Event).
+
+
+
+%%%% Application Runtime %%%%
+ensure_loaded(App) when is_atom(App) ->
+	case application:loaded(App) of
+		ok -> {ok, app_loaded};
+		{error, {already_loaded, _Name}} -> {ok, app_loaded};
+		Error -> {error, Error}
+	end.
+	
+ensure_deps_loaded([])-> ok;
+
+ensure_deps_loaded(Apps) when is_list(Apps)->
+	list:map( 
+		fun(App)-> 
+			case (ensure_loaded(App)) of 
+				{ok, app_loaded} -> {ok, app_loaded};
+				Error -> error_msg(?MODULE, ensure_deps_loaded, ?LINE, Error)
+			end
+		
+		end, 
+		Apps)
+	.
+	
+	
+%%%%%% Print %%%%%%%%%%%%%%%%%
+%print_list([])->ok;
+%print_list(L, Pattern) when is_list(L) ->
+
+format(Mod, Func, Line, Why) ->
+	io:format("Mod: ~w Func: ~w Line: ~w, Why: ~p",[Mod, Func, Line, Why]).
+		
+error_msg(Mod, Func, Line, Why) ->
+	error_logger:error_msg(format(Mod, Func, Line, Why)).		
