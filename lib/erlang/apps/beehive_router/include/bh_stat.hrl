1ee2777c538de76858cebd9dd8367716eaa442d8
ed t <edwardt.tril@gmail.com>
2011-01-12 11:51:52 -0800

    =update

--- lib/erlang/apps/beehive_router/include/bh_stat.hrl
+++ lib/erlang/apps/beehive_router/include/bh_stat.hrl
@@ -1,9 +1,11 @@
--type bh_counter-val()::pos_integer().
+-type bh_counter_val()::non_neg_integer().
+-type bh_stat_val():: number().
+
 -type bh_counter_name()::atom().
 
 -record(bh_stats, {
-	hit::bh_counter() =0,
-	sent::bh_counter()=0		
+	hit::bh_counter_val(),
+	sent::bh_stat_val()		
 	}).
 
 
--- lib/erlang/apps/beehive_router/src/bh_perf.erl
+++ lib/erlang/apps/beehive_router/src/bh_perf.erl
@@ -8,11 +8,10 @@
 -export([init/1, terminate/2, code_change/3,
 	handle_call/3, handle_cast/2, handle_info/2]).
 
-
 start()->
 	Config = ?defaultConfigSet,	
 	start(config).
 
 start(ConfigSet)->
-	.
+	ok.
 
