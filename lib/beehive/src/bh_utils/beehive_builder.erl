436b8c4c4d9e00e0dc9c4127e62ff45b0958a36e
Jacob Dunphy <jacob.dunphy@gmail.com>
2011-01-13 08:45:07 -0800

    Formatting cleanup.

--- lib/beehive/src/bh_utils/beehive_builder.erl
+++ lib/beehive/src/bh_utils/beehive_builder.erl
@@ -5,12 +5,12 @@
 -compile({no_auto_import,[error/1, error/2]}).
 
 -record (params, {
-  command   = undefined,
-  app_file,
-  root_dir  = undefined,
-  port      = 5001,
-  args      = []
-}).
+           command   = undefined,
+           app_file,
+           root_dir  = undefined,
+           port      = 5001,
+           args      = []
+          }).
 
 -export ([start/0]).
 
@@ -18,37 +18,43 @@ start() ->
   FullCommand = init:get_plain_arguments(),
 
   #params{
-    app_file = AppFile,
-    command = Command,
-    root_dir = RootDir1,
-    port      = Port,
-    args = CliArgs1
-    } = parse_args(FullCommand, #params{}),
+           app_file = AppFile,
+           command = Command,
+           root_dir = RootDir1,
+           port      = Port,
+           args = CliArgs1
+         } = parse_args(FullCommand, #params{}),
 
   App = build_app_from_args(AppFile),
 
   RootDir = case RootDir1 of
-    undefined ->
-      {ok, D} = file:get_cwd(),
-      D;
-    _ -> RootDir1
-  end,
+              undefined ->
+                {ok, D} = file:get_cwd(),
+                D;
+              _ -> RootDir1
+            end,
 
   application:set_env(beehive, home, RootDir),
 
-  % Build all the args here
-  ScratchDisk = config:search_for_application_value(scratch_dir, "/tmp/beehive/scratch"),
-  SquashedDisk = config:search_for_application_value(squashed_disk, "/tmp/beehive/squashed"),
-  BeeImage = filename:join([filename:absname(SquashedDisk), App#app.name, lists:append([App#app.name, ".bee"])]),
+  %% Build all the args here
+  ScratchDisk =
+    config:search_for_application_value(scratch_dir, "/tmp/beehive/scratch"),
+  SquashedDisk =
+    config:search_for_application_value(squashed_disk,
+                                        "/tmp/beehive/squashed"),
+  BeeImage =
+    filename:join([filename:absname(SquashedDisk),
+                   App#app.name,
+                   lists:append([App#app.name, ".bee"])]),
   HostIp = bh_host:myip(),
 
   CliArgs = [
-    {bee_image, BeeImage},
-    {port, Port},
-    {host, HostIp},
-    {scratch_dir, ScratchDisk},
-    {squashed_disk, SquashedDisk}
-  |CliArgs1],
+             {bee_image, BeeImage},
+             {port, Port},
+             {host, HostIp},
+             {scratch_dir, ScratchDisk},
+             {squashed_disk, SquashedDisk}
+             |CliArgs1],
 
   ok = validate_args(CliArgs),
   Bee = build_bee_from_args(App, CliArgs),
@@ -56,46 +62,51 @@ start() ->
   start_it_up(),
   case babysitter_integration:command(Command, App, Bee, CliArgs) of
     {ok, OsPid, ExitCode} ->
-io:format("
+      io:format("
   Ran the babysitter command: ~p
-    Ran with the OSPid of: ~p
-    Exit code: ~p
-  ", [Command, OsPid, ExitCode]);
+                Ran with the OSPid of: ~p
+                Exit code: ~p
+                ", [Command, OsPid, ExitCode]);
     {ok, ErlPid, OsPid, Model} ->
-io:format("
+      io:format("
   Successfully executed the command: ~p
-    Ran with the OSPid of: ~p and erlang pid: ~p
-    Bee id: ~p
-  ", [Command, OsPid, ErlPid, Model]);
+                             Ran with the OSPid of: ~p and erlang pid: ~p
+                             Bee id: ~p
+                             ", [Command, OsPid, ErlPid, Model]);
     {error, Phase, OsPid, ExitCode, StdOut, StdErr} ->
-io:format("
+      io:format("
   Command ~p failed at the stage: ~p
-    Ran with the OSPid of: ~p
-    Exit code: ~p
-    Stdout: ~s
-    Stderr: ~s
-  ", [Command, Phase, OsPid, ExitCode, StdOut, StdErr]);
+                                          Ran with the OSPid of: ~p
+                                          Exit code: ~p
+                                          Stdout: ~s
+                                          Stderr: ~s
+                                          ",
+                [Command, Phase, OsPid, ExitCode, StdOut, StdErr]);
   {error, no_command} ->
-io:format("
+                                             io:format("
   No command is described in the application type for this app~n
-", [])
+                                                       ", [])
   end,
-  shut_it_down().
+                                                       shut_it_down().
 
 %% INTERNAL
 
 start_it_up() ->
-  % application:start(sasl),
+  %% application:start(sasl),
   application:start(babysitter),
   node_manager:read_babysitter_config().
 
 shut_it_down() ->
   application:stop(babysitter).
 
-parse_args([ "-h" | _Rest], _Params) -> show_usage();
-parse_args([ "-a", AppFile | Rest], Params) -> parse_args(Rest, Params#params{app_file = AppFile});
-parse_args([ "-d", Dir | Rest], Params) -> parse_args(Rest, Params#params{root_dir = Dir});
-parse_args([ "-p", Port | Rest], Params) -> parse_args(Rest, Params#params{port = list_to_integer(Port)});
+parse_args([ "-h" | _Rest], _Params) ->
+  show_usage();
+parse_args([ "-a", AppFile | Rest], Params) ->
+  parse_args(Rest, Params#params{app_file = AppFile});
+parse_args([ "-d", Dir | Rest], Params) ->
+  parse_args(Rest, Params#params{root_dir = Dir});
+parse_args([ "-p", Port | Rest], Params) ->
+  parse_args(Rest, Params#params{port = list_to_integer(Port)});
 parse_args([H | Rest], #params{command = undefined} = Params) ->
   parse_args(Rest, Params#params{command = list_to_atom(H)});
 parse_args([H | Rest], #params{args = CurrArgs} = Params) ->
@@ -115,15 +126,15 @@ build_bee_from_args(#app{name = AppName, revision = Sha} = _App, Args) ->
   StartedAt = date_util:now_to_seconds(),
 
   #bee{
-    id                      = {AppName, Host, Port},
-    app_name                = AppName,
-    host                    = Host,
-    host_node               = node(self()),
-    port                    = Port,
-    status                  = pending,
-    revision             = Sha,
-    start_time              = StartedAt
-  }.
+        id                      = {AppName, Host, Port},
+        app_name                = AppName,
+        host                    = Host,
+        host_node               = node(self()),
+        port                    = Port,
+        status                  = pending,
+        revision             = Sha,
+        start_time              = StartedAt
+      }.
 
 fetch_file_proplist(Filename) ->
   case filelib:is_file(Filename) of
@@ -145,21 +156,21 @@ show_usage() ->
   io:format("
     Usage: beehive_control [OPTIONS] COMMAND
 
-    OPTIONS
-    -h                              Display this message
-    -a [file]                       File to load app descriptors
-    -b [file]                       File to load bee descriptors
-    -l                              Local node to connect
+            OPTIONS
+            -h                              Display this message
+            -a [file]                       File to load app descriptors
+            -b [file]                       File to load bee descriptors
+            -l                              Local node to connect
 
-    COMMANDS
-    stop                            Stop the beehive server entirely
-    app_updated [NameOfApp]         Marks an application as updated
-    set_seed [SeedNode]             Set a new seed
-    get_seed                        Get the seed node
-    reload                          Reload the beehive system
-    list [Type (optional)]          List types (valid types: router, storage, node or nothing)
+            COMMANDS
+            stop                            Stop the beehive server entirely
+            app_updated [NameOfApp]         Marks an application as updated
+            set_seed [SeedNode]             Set a new seed
+            get_seed                        Get the seed node
+            reload                          Reload the beehive system
+            list [Type (optional)]          List types (valid types: router, storage, node or nothing)
 
-", []),
+            ", []),
 halt(1).
 
 error(Msg) ->
@@ -168,10 +179,10 @@ error(Format, Args) ->
   Str = io_lib:format(Format, Args),
   io:format("
     *** ERROR ***
-    ~s
-", [Str]),
+              ~s
+            ", [Str]),
   show_usage(),
-  halt(2).
+            halt(2).
 
 validate_args([]) -> ok;
 validate_args([ {port, Port} | Rest]) ->
