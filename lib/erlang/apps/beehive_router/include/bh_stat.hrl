5bce36970e48fb4faa3907bb5af729222c574155
ed t <edwardt.tril@gmail.com>
2011-01-12 16:44:07 -0800

    =add

--- lib/erlang/apps/beehive_router/include/bh_stat.hrl
+++ lib/erlang/apps/beehive_router/include/bh_stat.hrl
@@ -1,12 +1,30 @@
--type bh_counter_val()::non_neg_integer().
--type bh_stat_val():: number().
--type bh_counter_name()::atom().
--type stat_enable()::boolean().
+-type counter_val()::non_neg_integer(). %hit counter value
 
--type protocol():: 'http' | 'https' | 'tcp' |'udp' |'http-alt'.
+-type stat_val():: number().  %collected statistic value
 
--define(Default_Conf, string:concat(erlang:atom_to_list(?MODULE),".conf")).
+-type counter_name()::atom(). %counter name
 
+-type stat_enable()::boolean(). %whether to enable that stat collection or not
+
+-type protocol():: 'http'  | 
+		   'https' | 
+		   'tcp' |
+		   'udp' |
+		   'http-alt'. %supported protocol
+
+-type timestamp() :: non_neg_integer().  % microseconds with a special epoch
+
+-type bh_stat_option():: 
+
+-record (counter, 
+	{name::counter_name(), 
+	 increment = undefined
+	 }).
+	 
+-record (rate,
+	{
+	
+	}).
 
 -record(bh_stats, {
 	hit::bh_counter_val(),
@@ -14,3 +32,4 @@
 	}).
 
 
+-define(Default_Conf, string:concat(erlang:atom_to_list(?MODULE),".conf")).
