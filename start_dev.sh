111f64096bd910a980fda4cf9a405df9bf610856
Pete Elmore <pete@debu.gs>
2011-01-24 17:09:17 -0800

    Tab mixture.  That was probably me, but also maybe not.  I don't know.

--- start_dev.sh
+++ start_dev.sh
@@ -37,5 +37,5 @@ eval "erl \
     -eval \"application:start(sasl, [{errlog_type, error}])\" \
     -eval \"application:start(os_mon)\" \
     -eval \"application:start(crypto)\" \
-		-s reloader \
+    -s reloader \
     -eval \"application:start(beehive)\""
