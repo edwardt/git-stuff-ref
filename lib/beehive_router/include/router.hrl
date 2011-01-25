7a7da3959d6333c9ddad1b13e29f40e3a1001b2d
ed t <edwardt.tril@gmail.com>
2011-01-24 17:06:34 -0800

    =a bit unit test

--- lib/beehive_router/include/router.hrl
+++ lib/beehive_router/include/router.hrl
@@ -11,6 +11,25 @@
 		   'udp' |
 		   'http-alt'. %supported protocol
 
+-define (http_ok_200, <<"">>).
+-define (http_accepted_202, <<>>).
+-define (http_bad_request_400, <<>>).
+-define (http_unauthorized_401, <<>>).
+-define (http_forbidden_403, <<>>).
+-define (http_method_not_allowed_405, <<>>).
+-define (http_request_timedout_408, <<>>).
+-define (http_conflict_409, <<>>).
+-define (http_gone_410, <<>>).
+-define (http_request_entity_too_large_413, <<>>).
+-define (http_unspported_media_type_415, <<>>).
+-define (http_internal_server_error_500, <<>>).
+-define (http_not_implemented_501, <<"HTTP/1.1 501 Not Implemented\r\n\r\n">>).
+-define (http_service_unavailable_503, <<>>).
+
+
+
+
+
 
 
 
